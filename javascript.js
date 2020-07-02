f3c9bc2bed7e2c591c4f31ab589b9a86b4596df1
Alex Madrigal <jam.madrigal@gmail.com>
2020-07-02 13:43:35 -0700

    added an event listener for the start button as well as a function that will start the quiz when finished

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Identifying the start button in the html and adding a listener for clicking it
+var startBtn = document.getElementById("startbtn");
+
+startBtn.addEventListener("click", startQuiz);
+
+// Starts the quiz
+function startQuiz() {
+}
+
