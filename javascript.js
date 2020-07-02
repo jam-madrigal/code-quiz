5bcbcab07d3f70bcafbaf1da47602a807bc262cc
Alex Madrigal <jam.madrigal@gmail.com>
2020-07-02 13:59:52 -0700

    made the firstquestion function edit the h2 tag of the questions div

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ var startBtn = document.getElementById("startbtn");
 startBtn.addEventListener("click", startQuiz);
 
 // Identifying the element that holes the question and quiz explanations
-var questionsElement = document.getElementById("questions");
+var questionsElement = document.getElementById("qh2");
 
 // Identifying the element that holds the responses
 var answersElement = document.getElementById("responses");
@@ -14,5 +14,11 @@ var answersElement = document.getElementById("responses");
 function startQuiz() {
     startBtn.classList.add("hidden");
     questionsElement.textContent="";
+
+    firstQuestion();
 }
 
+// Displays the first question
+function firstQuestion() {
+    questionsElement.textContent="What is x?"
+}
